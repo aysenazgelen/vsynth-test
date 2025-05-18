09db26a1cb3b28f73a58103511541fb860dc27b3
aysenazgelen <aysenazgelen@gmail.com>
2025-05-18 16:51:36 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ayşenaz Gelen on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
